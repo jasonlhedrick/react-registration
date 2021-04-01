4aaa82c47232a17a61696414d38ee1ff631c206b
Jason Hedrick <jasonleehedrick@gmail.com>
2021-03-31 17:41:03 -0700

    Properly sending and receiving information from the basic server endpoints. Need to fill out some more of the backend now to finalize registration.

--- src/App.js
+++ src/App.js
@@ -1,10 +1,42 @@
+import React, { useState } from 'react';
+import axios from 'axios';
 import './App.css';
 
+const serverLoc = 'http://localhost:4000'
+
+function register(username, password, email) {
+  axios.post(`${serverLoc}/user`, 
+  {'username': username, 'password': password, 'email': email})
+  .then(res => {
+    console.log(res.data);
+  })
+  .catch(err => {
+    console.log(err);
+  });
+}
+
 function App() {
+  const [username, setUsername] = useState('');
+  const [password, setPassword] = useState('');
+  const [email, setEmail] = useState('');
+
   return (
     <main className="App">
+      <form onSubmit={(e) => {
+        e.preventDefault();
+        register(username, password, email)}
+        }>
+        <input type="text" name="username" 
+        onChange={e => setUsername(e.target.value)}></input>
+        <input type="password" name="password" 
+        onChange={e => setPassword(e.target.value)}></input>
+        <input type="email" name="email" 
+        onChange={e => setEmail(e.target.value)}></input>
+        <button type="submit">Register</button>
+      </form>
     </main>
   );
 }
+
 
 export default App;
