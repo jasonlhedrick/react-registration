664eb849741a0f7ff2e6f880673ccb5e2f55b09c
Jason Hedrick <jasonleehedrick@gmail.com>
2021-04-01 17:06:44 -0700

    Worked on making the form pop more. Adding in some micro-interactions to show to the user the server is working on what they submitted.

--- src/App.js
+++ src/App.js
@@ -5,14 +5,22 @@ import './App.css';
 const serverLoc = 'http://localhost:4000'
 
 function register(username, password, email) {
+  const registerButton = document.querySelector("#registerButton");
+  registerButton.disabled = true;
+  registerButton.style.cursor = 'wait';
   axios.post(`${serverLoc}/user`, 
   {'username': username, 'password': password, 'email': email})
   .then(res => {
     console.log(res.data);
+    registerButton.disabled = false;
+    registerButton.style.cursor = 'auto';
   })
   .catch(err => {
-    console.log(err);
+    if (err.response) console.log(err.response.data);
+    registerButton.disabled = false;
+    registerButton.style.cursor = 'auto';
   });
+  
 }
 
 function App() {
@@ -22,17 +30,23 @@ function App() {
 
   return (
     <main className="App">
-      <form onSubmit={(e) => {
+      <form className="registrationForm" onSubmit={(e) => {
         e.preventDefault();
         register(username, password, email)}
         }>
-        <input type="text" name="username" 
-        onChange={e => setUsername(e.target.value)}></input>
-        <input type="password" name="password" 
-        onChange={e => setPassword(e.target.value)}></input>
-        <input type="email" name="email" 
-        onChange={e => setEmail(e.target.value)}></input>
-        <button type="submit">Register</button>
+          <fieldset>
+            <legend>Register an account</legend>
+            <label for="username">Username</label>
+            <input type="text" name="username" 
+            onChange={e => setUsername(e.target.value)}></input>
+            <label for="password">Password</label>
+            <input type="password" name="password" 
+            onChange={e => setPassword(e.target.value)}></input>
+            <label for="email">Email</label>
+            <input type="email" name="email" 
+            onChange={e => setEmail(e.target.value)}></input>
+            <button id="registerButton" type="submit">Register</button>
+        </fieldset>
       </form>
     </main>
   );
